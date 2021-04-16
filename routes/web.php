92b234b5e017b4bec8d13b688415f9415a0897da
Farid Aziz <faridazizw@gmail.com>
2021-04-17 05:25:29 +0700

    home

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.app');
 });
+
+Route::get('/home', \App\Http\Livewire\Home::class);
+
+// Route::get('/login', \App\Http\Livewire\Login::class);
